4cdecc3068156869acc5e38a0a164b30ef9d4ad2
wsg-12 <gautam.wise.sparkz@gmail.com>
2024-11-13 19:22:43 +0530

    Router configured

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import CreateTrip from './create-trip'
+
+
+const router =createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    // component: App,
+    // exact: true,
+  },
+  {
+    path: '/create-trip',
+    element: <CreateTrip/>,
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
