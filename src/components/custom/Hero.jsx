295a489134fa30f44c20d4e0e4b3e4beb3b06f65
wsg-12 <gautam.wise.sparkz@gmail.com>
2024-11-14 14:30:08 +0530

    google api added

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 export default function Hero() {
   return (
@@ -7,8 +8,9 @@ export default function Hero() {
         <h1 className='font-extrabold text-[50px] text-center mt-16'> <span className='text-[#f56551]'>Discover Your trips with AI:</span> Powerful Personalized trip planner</h1>
 
         <p className='text-xl text-gray-500 text-center'> Powerful Personalized trip planner making your trips hazle free and enjoying with no worries and with planned budget . </p>
-
-        <Button>Get Started, Its Free</Button>
+        <Link to="/create-trip">
+            <Button>Get Started, Its Free</Button>
+        </Link>
     </div>
   )
 }
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 function CreateTrip() {
+  const [place , setPlace]= useState();
   return (
-    <div>CreateTrip</div>
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'> 
+        <h2 className='font-bold text-3xl'>
+            Tell us your travel preferences
+        </h2>
+        <p className='mt-3 text-gray-500 text-xl'> just provide some basic information and our trip planner will take care the rest </p>
+        <div className='mt-20'>
+            <div> 
+                <h2 className='text-xl my-3 font-medium'> What is your destination of choice?</h2>     
+                <GooglePlacesAutocomplete
+                    apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+                    selectProps={{place, onChange:(v)=>{setPlace(v);}}}
+                    />
+            </div>
+        </div>
+    </div>
   )
 }
 
