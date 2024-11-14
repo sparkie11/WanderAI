b6f7691c16b4cd3a312f875ff199a624da3973b1
sparkie11 <gautamwise.s@gmail.com>
2024-11-14 15:42:37 +0530

    form values set

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -72,7 +72,7 @@ function CreateTrip() {
           <div className='grid grid-cols-3 gap-5 mt-5 '>
             {SelectTravelList.map((item,index)=> (<div key={index} onClick={()=>handleInputChange('traveler',item.title)} className={`p-4 border rounded-lg cursor-pointer hover:shadow-lg ${formData?.traveler==item.title && 'shadow-lg border border-red-400'}`}>
               <h2 className='text-4xl'> {item.icon}</h2>
-              <h2 className='font-bold text-lg'>{item.title}</h2>
+              <h2 className='font-bold text-lg'>{item.title}</h2> 
               <h2 className='text-sm text-gray-500'>{item.desc}</h2>
 
 
