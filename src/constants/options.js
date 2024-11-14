997cfc695f58561a46bfef28aba79ae9737b3448
wsg-12 <gautam.wise.sparkz@gmail.com>
2024-11-14 14:51:03 +0530

    details page

---
+++ src/constants/options.js
@@ -0,0 +1,70 @@
+export const SelectTravelList=[
+    {
+        id: 1,
+        title: 'Just one',
+        desc: "A sole travel",
+        icon: '#',
+        people: '1'
+    },
+    {
+        id: 2,
+        title: 'A couple',
+        desc: "A couple's travel",
+        icon: '#',
+        people: '2'
+    },
+    {
+        id: 3,
+        title: 'A group',
+        desc: "A group's travel",
+        icon: '#',
+        people: '3+'
+    },
+    {
+        id: 4,
+        title: 'Family',
+        desc: "A family's travel",
+        icon: '#',
+        people: '4+'
+    },{
+        id: 5,
+        title: 'Couple and family',
+        desc: "A couple and family's travel",
+        icon: '#',
+        people: '4+'
+    }
+]
+
+
+export const SelectBudgetOptions=[
+    {
+        id: 1,
+        title: 'cheap',
+        desc: "stay conscious of cost",
+        icon: '#',
+    },{
+        id: 2,
+        title: 'budget-friendly',
+        desc: "find a balance between cost and quality",
+        icon: '#',
+    },
+    {
+        id: 3,
+        title: 'expensive',
+        desc: "stay on top of the cost",
+        icon: '#',
+    },
+    {
+        id: 4,
+        title: 'luxury',
+        desc: "stay in the best possible location",
+        icon: '#',
+    },
+    {
+        id: 5,
+        title: 'ultra-luxury',
+        desc: "stay in the most beautiful location",
+        icon: '#',
+    }
+
+]
