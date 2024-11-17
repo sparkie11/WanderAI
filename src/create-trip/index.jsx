4d28ee086c7dc6a26e90010fcb408ce5b2892a29
sparkie11 <gautamwise.s@gmail.com>
2024-11-17 15:47:45 +0530

    plan

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -35,7 +35,7 @@ function CreateTrip() {
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'> 
         <h2 className='font-bold text-3xl'>
-            Tell us your travel preferences 🏕️
+            Tell us your travel preferences  🏕️
         </h2>
         <p className='mt-3 text-gray-500 text-xl'> just provide some basic information and our trip planner will take care the rest </p>
         <div className='mt-20 flex flex-col gap-10'>
